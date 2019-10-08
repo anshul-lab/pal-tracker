35c3f1b62ba336d12d50069deeb3b92870c8f2b9
Anshul <anshulgupta2.tcs@gmail.com>
2019-10-08 14:51:34 +0530

    Done till Assignment 2

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,36 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private String PORT;
+    private String MEMORY_LIMIT;
+    private String CF_INSTANCE_INDEX;
+    private String CF_INSTANCE_ADDR;
+
+    public EnvController(@Value("${port:NOT SET}") String PORT,
+                         @Value("${memory.limit:NOT SET}") String MEMORY_LIMIT,
+                         @Value("${cf.instance.index:NOT SET}") String CF_INSTANCE_INDEX,
+                         @Value("${cf.instance.addr:NOT SET}") String CF_INSTANCE_ADDR) {
+        this.PORT = PORT;
+        this.MEMORY_LIMIT = MEMORY_LIMIT;
+        this.CF_INSTANCE_INDEX = CF_INSTANCE_INDEX;
+        this.CF_INSTANCE_ADDR = CF_INSTANCE_ADDR;
+    }
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        Map<String, String> envMap = new HashMap<String, String>();
+        envMap.put("PORT",PORT);
+        envMap.put("MEMORY_LIMIT",MEMORY_LIMIT);
+        envMap.put("CF_INSTANCE_INDEX",CF_INSTANCE_INDEX);
+        envMap.put("CF_INSTANCE_ADDR",CF_INSTANCE_ADDR);
+        return envMap;
+    }
+}
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+    String message = "hello";
+
+    public WelcomeController(@Value("${welcome.message}") String message) {
+        this.message = message;
+    }
+
     @RequestMapping(method=RequestMethod.GET, value="/")
     public String hello(){
-        return "hello";
+        return message;
     }
 }
